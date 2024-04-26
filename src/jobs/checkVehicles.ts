f217016ead18fd0a2e275f584f15c8bb6259a06e
Pablo Tanner <15524263+pablotanner@users.noreply.github.com>
2024-04-26 15:15:45 +0200

    Some more structure stuff

---
+++ src/jobs/checkVehicles.ts
@@ -0,0 +1,1 @@
+/* Code that contains the logic for job where we check vehicle telematic data for all cars */
---
+++ src/services/dataService.ts
@@ -0,0 +1,5 @@
+
+/*
+Contain methods for the telematic data items, e.g. unifying or generating Drizzle objects from them
+
+*/
